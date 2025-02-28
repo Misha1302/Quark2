04303c3934f8b8baf67a1aebc4e90d387fc10ad6
Михаил <77919295+Misha1302@users.noreply.github.com>
2025-02-28 11:27:24 +0300

    dynamic method calls benchmarks added

---
+++ TranslatorBenchmarks/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Emit;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using GrEmit;
+using RuntimeHackes;
+
+BenchmarkRunner.Run<TranslatorTest>();
+
+public class TranslatorTest
+{
+    private const int A = 2;
+    private const int B = 3;
+
+    private Func<int, int, int> _delegate = null!;
+    private DynamicMethod _dynamicMethod = null!;
+    private nint _ptr;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _dynamicMethod = new DynamicMethod(
+            "Sum",
+            typeof(int),
+            [typeof(int), typeof(int)],
+            true
+        );
+
+        using var il = new GroboIL(_dynamicMethod);
+
+        il.Ldarg(0);
+        il.Ldarg(1);
+        il.Add();
+        il.Ret();
+
+        _delegate = _dynamicMethod.CreateDelegate<Func<int, int, int>>();
+        _ptr = _dynamicMethod.GetNativePointer();
+    }
+
+
+    [Benchmark]
+    public int DynamicMethodCall() => (int)_dynamicMethod.Invoke(null, [A, B])!;
+
+    [Benchmark]
+    public int DelegateCall() => _delegate(A, B);
+
+    [Benchmark(Baseline = true)]
+    public unsafe int PointerCall() => ((delegate*<int, int, int>)_ptr)(A, B);
+}
