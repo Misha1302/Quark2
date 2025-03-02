7de2d493554648d5cfb50589f7cf3e668b350770
Михаил <77919295+Misha1302@users.noreply.github.com>
2025-03-03 02:09:15 +0300

    call optimization

--- TranslatorToMsil/RuntimeLibrary.cs
+++ TranslatorToMsil/RuntimeLibrary.cs
@@ -1,17 +1,24 @@
+using System.Runtime.CompilerServices;
+
 namespace TranslatorToMsil;
 
 public static class RuntimeLibrary
 {
     public static TranslatorRuntimeData RuntimeData = null!;
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool ToBool(in AnyOpt value) => value.IsTrue();
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static AnyOpt GetConst(int index) => RuntimeData.Constants[index];
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static unsafe AnyOpt CallFunc(string name) =>
         ((delegate*<TranslatorRuntimeData, AnyOpt>)RuntimeData.DynamicMethods[name])(RuntimeData);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void PushToStack(in AnyOpt value) => RuntimeData.IntermediateData.Push(value);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static AnyOpt PopFromStack() => RuntimeData.IntermediateData.Pop();
 }
